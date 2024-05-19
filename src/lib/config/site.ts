71bce61d959f9f98d797884a4d9cf6f2ed7271d4
Pro-Palestine <pro.palestine.324@gmail.com>
2024-05-19 15:34:16 +0000

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -2,16 +2,16 @@ import type { SiteConfig } from '$lib/types/site'
 
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
-  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
-  title: 'Urara',
-  subtitle: 'Sweet & Powerful SvelteKit Blog Template',
+  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'propalestine.netlify.app',
+  title: 'Pro Palestine',
+  subtitle: 'An Idea to help Palestinians',
   lang: 'en-US',
-  description: 'Powered by SvelteKit/Urara',
+  description: '',
   author: {
     avatar: '/assets/maskable@512.png',
-    name: 'John Doe',
-    status: '🌸',
-    bio: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    name: 'Hany',
+    status: '🇵🇸',
+    bio: 'A computer science Paki student trying to do something. This site is under production'
   },
   themeColor: '#3D4451'
 }
